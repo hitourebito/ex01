25c6c91939b4968ac3e364f2e21c4b716d4cdc87
hitourebito <yuto0423777@gmail.com>
2019-08-21 19:10:21 +0900

    Initial commit

---
+++ ex02.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <?php
+      $a = 1234;
+      $b = 23.4;
+
+      echo "${a} + ${b} = " . ($a + $b), "<br/>";
+    ?>
+    
+
+    </p>
+  </body>
+</html>
